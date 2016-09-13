b31eaa62a6ae0a77fffec7021b5dd2c52a083dfb
Qin Jiangbo <qinjiangbo@users.noreply.github.com>
2016-09-13 16:14:12 +0800

    create packages for mybatis

---
+++ src/com/qinjiangbo/pojo/User.java
@@ -0,0 +1,74 @@
+package com.qinjiangbo.pojo;
+
+/**
+ * Date: 9/13/16
+ * Author: qinjiangbo@github.io
+ */
+public class User {
+
+    // 实体类的属性名称和数据库中的字段名称要一一对应起来
+    private long id;
+    private String name;
+    private String pwd;
+    private int age;
+    private String email;
+    private String tel;
+    private String addr;
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPwd() {
+        return pwd;
+    }
+
+    public void setPwd(String pwd) {
+        this.pwd = pwd;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getTel() {
+        return tel;
+    }
+
+    public void setTel(String tel) {
+        this.tel = tel;
+    }
+
+    public String getAddr() {
+        return addr;
+    }
+
+    public void setAddr(String addr) {
+        this.addr = addr;
+    }
+
+}
