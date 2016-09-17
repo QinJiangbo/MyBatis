a21f81d00ea13fa4089a5c9b2aed9eb52ef22e9d
Qin Jiangbo <qinjiangbo@users.noreply.github.com>
2016-09-17 08:32:25 +0800

    add classes and teacher info

--- src/com/qinjiangbo/dao/ClassesMapper.java
+++ src/com/qinjiangbo/dao/ClassesMapper.java
@@ -1,9 +1,18 @@
 package com.qinjiangbo.dao;
 
+import com.qinjiangbo.pojo.Classes;
+
 /**
  * Date: 9/16/16
  * Author: qinjiangbo@github.io
  */
 public interface ClassesMapper {
 
+    /**
+     * 根据编号查找班级信息
+     *
+     * @param id
+     * @return
+     */
+    public Classes findClassById(int id);
 }
---
+++ src/com/qinjiangbo/test/MyBatisTest4.java
@@ -0,0 +1,40 @@
+package com.qinjiangbo.test;
+
+import com.qinjiangbo.dao.ClassesMapper;
+import com.qinjiangbo.pojo.Classes;
+import org.apache.ibatis.session.SqlSession;
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.apache.ibatis.session.SqlSessionFactoryBuilder;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import java.io.InputStream;
+
+/**
+ * Date: 9/17/16
+ * Author: qinjiangbo@github.io
+ */
+public class MyBatisTest4 {
+    private SqlSession sqlSession = null;
+
+    @Before
+    public void init() {
+        String config = "com/qinjiangbo/conf/configure.xml";
+        InputStream inputStream = MyBatisTest2.class.getClassLoader().getResourceAsStream(config);
+        SqlSessionFactory sqlSessionFactory = new SqlSessionFactoryBuilder().build(inputStream);
+        sqlSession = sqlSessionFactory.openSession();
+    }
+
+    @Test
+    public void testFindClassById() {
+        ClassesMapper classesMapper = sqlSession.getMapper(ClassesMapper.class);
+        Classes classes = classesMapper.findClassById(1);
+        System.out.println(classes);
+    }
+
+    @After
+    public void commit() {
+        sqlSession.commit();
+    }
+}
