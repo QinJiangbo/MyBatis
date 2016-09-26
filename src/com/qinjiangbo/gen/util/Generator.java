90c7dfa5737100c86c2e67cfbf6e1c7e0a598712
QinJiangbo <qinjiangbo@users.noreply.github.com>
2016-09-26 22:56:43 +0800

    add mybatis-generator-core library

---
+++ src/com/qinjiangbo/gen/util/Generator.java
@@ -0,0 +1,29 @@
+package com.qinjiangbo.gen.util;
+
+import org.mybatis.generator.api.MyBatisGenerator;
+import org.mybatis.generator.config.Configuration;
+import org.mybatis.generator.config.xml.ConfigurationParser;
+import org.mybatis.generator.internal.DefaultShellCallback;
+
+import java.io.File;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by Richard on 6/20/16.
+ */
+public class Generator {
+
+    public static void main(String[] args) throws Exception {
+        List<String> warnings = new ArrayList<String>();
+        boolean overwrite = true;
+        String userDir = System.getProperty("user.dir");
+        File configFile = new File(userDir + "/src/tools/resources/generatorConfiguration.xml");
+        ConfigurationParser cp = new ConfigurationParser(warnings);
+        Configuration config = cp.parseConfiguration(configFile);
+        DefaultShellCallback callback = new DefaultShellCallback(overwrite);
+        MyBatisGenerator myBatisGenerator = new MyBatisGenerator(config, callback, warnings);
+        myBatisGenerator.generate(null);
+        System.out.println("Completed!");
+    }
+}
